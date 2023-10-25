f8c7eff19b924602037956eeeda27c24da27e7bb
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2023-10-25 15:55:29 -0300

    :sparkles: feat: Adding the seventh question

---
+++ java/Qst7.java
@@ -0,0 +1,15 @@
+public class Qst7 {
+    public static void main (String [] args){
+        int num = 2;
+
+        while(num != 1){
+            if (num % 2 == 0){
+                num = num / 2;
+                System.out.println(num);
+            } else {
+                num = 3 * num + 1;
+                System.out.println(num);
+            }
+        }
+    }
+}
